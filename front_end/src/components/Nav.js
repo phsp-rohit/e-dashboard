b10b0dc4eb3a3f1ce47d85bffdd0053fc4f12a41
RohitPahasp <rohit@pahasp.com>
2026-02-04 15:16:24 +0530

    ReactJs Navbar Update

--- front_end/src/components/Nav.js
+++ front_end/src/components/Nav.js
@@ -4,25 +4,43 @@ import { Link, useNavigate } from 'react-router-dom';
 const Nav = () => {
   const auth = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
+
   const logout = () => {
     localStorage.clear();
     navigate('/signup');
-  }
+  };
 
   return (
     <ul className="nav-ul">
-      <li><Link to="/">Products</Link></li>
-      <li><Link to="/add">Add Product</Link></li>
-      <li><Link to="/update">Update Product</Link></li>
-      <li><Link to="/profile">Profile</Link></li>
+      {/* LOGO */}
+      <li className="logo-li">
+        <img
+          src="https://simicart.com/wp-content/uploads/eCommerce-logo.jpg"
+          alt="logo"
+          className="logo"
+        />
+      </li>
 
-         {
-           auth ?<li><Link onClick={logout} to="/signup">Logout</Link></li> :
-           <>
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/login">Login</Link></li></>
-         }
-      
+      {auth ? (
+        <>
+          <li><Link to="/">Products</Link></li>
+          <li><Link to="/add">Add Product</Link></li>
+          <li><Link to="/update">Update Product</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+
+          <li className="nav-spacer"></li>
+
+          <li>
+            <Link onClick={logout}>Logout ({auth.name})</Link>
+          </li>
+        </>
+      ) : (
+        <>
+          <li className="nav-spacer"></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </>
+      )}
     </ul>
   );
 };
