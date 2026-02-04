305cac116ae6f30f696549025d47606840349754
RohitPahasp <rohit@pahasp.com>
2026-02-04 10:47:51 +0530

    Login API in NodeJs

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 require('./db/config');
-const users = require('./db/users');
+const Users = require('./db/users');
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.post("/register", async (req, res) => {
-  let user = new users(req.body);
+  let user = new Users(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
 });
 
+app.post("/login", async (req, res) => {
+  if (req.body.email && req.body.password) {
+    let user = await Users.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No User found" });
+    }
+  } else {
+    res.send({ result: "No User found" });
+  }
+});
 
-
-app.listen(5000) ;
+app.listen(5000);
