4a7b220077a8254cf1caca60943ac43fba2b8661
RohitPahasp <rohit@pahasp.com>
2026-02-09 13:08:32 +0530

    Prefill update form

--- front_end/src/components/UpdateProduct.js
+++ front_end/src/components/UpdateProduct.js
@@ -1,32 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateProduct = () => {
-    const [name,setName] = React.useState("");
-    const [price,setPrice] = React.useState("");
-    const [category,setCategory] = React.useState("");
-    const [company,setCompany] = React.useState("");
-    
-    const UpdateProduct = async() => {
-        console.warn(name,price,category,company);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [company, setCompany] = useState("");
 
-       
-    }
-        
-    return( 
+  const params = useParams();
+
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+
+  const getProductDetails = async () => {
+    console.warn(params);
+    let result = await fetch(`http://localhost:5000/product/${params.id}`);
+    result = await result.json();
+    setName(result.name);
+    setPrice(result.price);
+    setCategory(result.category);
+    setCompany(result.company);
+  };
+
+  const updateProduct = async () => {
+    console.warn(name, price, category, company);
+  };
+
+  return (
     <div className="add-product">
-        <h1>Update Product </h1>
-        <input type="text" placeholder="Enter Product Name" value={name} onChange={(e)=>{setName(e.target.value)}} />
-        
-        <input type="text" placeholder="Enter Product Price" value={price} onChange={(e)=>{setPrice(e.target.value)}} />
-        
-        <input type="text" placeholder="Enter Product Category" value={category} onChange={(e)=>{setCategory(e.target.value)}} />
-        
-        <input type="text" placeholder="Enter Product Company" value={company} onChange={(e)=>{setCompany(e.target.value)}} />
-        
-        <button onClick={UpdateProduct} className="add-btn">Update Product</button>
-    </div>
-    );
-} 
+      <h1>Update Product </h1>
 
+      <input
+        type="text"
+        placeholder="Enter Product Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter Product Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter Product Category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter Product Company"
+        value={company}
+        onChange={(e) => setCompany(e.target.value)}
+      />
+
+      <button onClick={updateProduct} className="add-btn">
+        Update Product
+      </button>
+    </div>
+  );
+};
 
 export default UpdateProduct;
