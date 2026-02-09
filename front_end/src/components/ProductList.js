67e8324d70e6fa3b6e60fb065be1790f6646738d
RohitPahasp <rohit@pahasp.com>
2026-02-09 14:50:04 +0530

    Handle Auth Token in React js

--- front_end/src/components/ProductList.js
+++ front_end/src/components/ProductList.js
@@ -9,7 +9,11 @@ const ProductList = () => {
   }, []);
 
   const getProducts = async () => {
-    let result = await fetch("http://localhost:5000/products");
+    let result = await fetch("http://localhost:5000/products", {
+      headers : {
+        authorization : `bearer ${JSON.parse(localStorage.getItem("token"))}`
+    }
+    });
     result = await result.json();
     setProducts(result);
   };
--- front_end/src/components/signup.js
+++ front_end/src/components/signup.js
@@ -21,7 +21,8 @@ const SignUp = () => {
       headers: { "Content-Type": "application/json" }
     });
     result = await result.json();
-    localStorage.setItem("user", JSON.stringify(result));
+    localStorage.setItem("user", JSON.stringify(result.result));
+    localStorage.setItem("token", JSON.stringify(result.auth));
     if (result) navigate('/');
   };
 
